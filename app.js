3745e18aa0fe0370aeddaa1f402c96c06464ef4a
juanfer3 <jfvilladiego2@misena.edu.co>
2017-10-18 22:57:02 -0500

    my primer commit!

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+ *Module dependencies
+ */
+import React from 'react';
+import PokeTable from './components/PokeTable';
+
+var pokemons = [
+  {number:1, name: 'Bulbasaur'},
+  {number:7, name: 'Ivysaur'},
+  {number:4, name: 'Venusaur'}
+];
+
+React.render(
+  <PokeTable pokemons={pokemons} />,
+  document.getElementById('container')
+);
